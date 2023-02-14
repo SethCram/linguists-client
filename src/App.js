f5ae04c394282825957ebf93e5b7acfa6c8e1c0b
sethhi2 <sethcram@gmail.com>
2023-02-13 19:52:39 -0800

    loading cursor enabled when submit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 import { useEffect, useState } from 'react';
 import Dropdown from 'react-dropdown';
 import 'react-dropdown/style.css';
+import Header from './components/header/Header';
 
 //for some reson, not enough to set proxy in package.json
 axios.defaults.baseURL = "http://localhost:8000/";
@@ -17,7 +18,8 @@ function App() {
   const [isFetching, setIsFetching] = useState(false);
   const [isUploading, setIsUploading] = useState(false);
   const [question, setQuestion] = useState("");
-  
+  const [askedQuestion, setAskedQuestion] = useState("");
+  const [queriedDb, setQueriedDb] = useState(null);
 
   useEffect(() => { //cant detch data in here since using sync funct
     
@@ -66,6 +68,8 @@ function App() {
         question: question
       });
 
+      setQueriedDb(selectedDbName);
+      setAskedQuestion(question);
       setSql(response.data[0].query); //not sure why returns arr of results
       setResults(response.data[0].execution_results);
     }
@@ -105,68 +109,81 @@ function App() {
   };
 
   return (
-    <section id="app">
-      <h5>Capstone-Linguists</h5>
-      <h2>Frontend UI</h2>
+    <div id='app' className={(isFetching || isUploading) && 'loading'}>
+      <Header/>
+      <section id="userinput">
+        <h5>Interact with the System</h5>
+        <h2>User Input</h2>
 
-      <form
-        className='container app__container'
-        onSubmit={handleSubmit}
-      >
-        <div className='app__container__databases'>
-          <label htmlFor="app__container__databases__file__input">
-            <i className="app__container__databases__upload__icon fa-solid fa-plus"></i>
-          </label>
-          <input
-              type="file"
-              id="app__container__databases__file__input"
-              style={{ display: "none" }} 
-              onChange={file=>{handleFileUpload(file.target.files[0])}}
-          />
-          <Dropdown
-            className='app__container__databases__dropdown'
-            options={dbNames}
-            onChange={(fileObj) => { setSelectedDbName(fileObj.value) }}
-            value={selectedDbName}
-            placeholder="Select a database to query..."
-          />
-        </div>
-        <input
-          className='app__container__question'
-          type="text"
-          placeholder='Enter a request..'
-          onChange={(event) => { setQuestion(event.target.value); }}
-        />
-        {sql && 
-          <div className='app__container__sql'>
-            <label>SQL Generated</label>
-            <p>{sql}</p>
-          </div>
-        }
-        {Array.isArray(results) && results.length !== 0 && //only display if results available
-          <div className='app__container__rslts'>
-            <label>SQL Results</label>
-            <ul>
-              {results.map((rslt, i) => (
-                <li key={i}>{rslt}</li>
-              ))}
-            </ul>
-          </div>
-        }
-        <button
-          className="btn btn-primary app__container__submit"
-          type="submit"
-          disabled={isFetching || isUploading || !question || !selectedDbName} //disable btn if fetching, uploading, or one of inputs not set
+        <form
+          className='container userinput__container'
+          onSubmit={handleSubmit}
         >
-          Search
-        </button>
-      </form>
+          <div className='userinput__databases'>
+            <label htmlFor="userinput__databases__file__input">
+              <i className="userinput__databases__upload__icon fa-solid fa-plus"></i>
+            </label>
+            <input
+                type="file"
+                id="userinput__databases__file__input"
+                style={{ display: "none" }} 
+                onChange={file=>{handleFileUpload(file.target.files[0])}}
+            />
+            <Dropdown
+              className='userinput__databases__dropdown'
+              options={dbNames}
+              onChange={(fileObj) => { setSelectedDbName(fileObj.value) }}
+              value={selectedDbName}
+              placeholder="Select a database to query..."
+              required //doesnt work in dropdown
+            />
+          </div>
+          <textarea
+            name='message'
+            rows='7'
+            placeholder='Enter a request...'
+            onChange={(event) => setQuestion(event.target.value)}
+            required />
+          <button
+            className="btn btn-primary userinput__submit"
+            type="submit"
+            disabled={isFetching || isUploading || !question || !selectedDbName} //disable btn if fetching, uploading, or one of inputs not set
+          >
+            Search
+          </button>
+        </form>
+      </section>
+
+      <section id="output">
+        <h5>{askedQuestion ? askedQuestion : "User Requested"}</h5>
+        <h2>Results</h2>
+
+        <div className='container output__container'>
+          {sql && 
+            <div className='output__item output__sql'>
+              <h3><b>Generated SQL</b></h3>
+              <p>{sql}</p>
+            </div>
+          }
+          {Array.isArray(results) && results.length !== 0 && //only display if results available
+            <div className='output__item output__rslts'>
+              <h3><b>Information from {queriedDb} Database</b></h3>
+              <ul className='output__rslts__list'>
+                {results.map((rslt, i) => (
+                  <li className='output__rslts__list__elly' key={i}>{rslt}</li>
+                ))}
+              </ul>
+            </div>
+          }
+        </div>
+      </section>
+
       {selectedDbName &&
         <>
           {/* Give a description of the selected database + show it in a graphical fashion */ }
         </>
       }
-    </section>
+    </div>
   );
 }
 
